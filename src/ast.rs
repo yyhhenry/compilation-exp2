52ec9bca927d1ef5af906787e8dba133f01ccf11
yyhhenry <yyhhenry@foxmail.com>
2024-04-29 23:19:01 +0800

    AST

---
+++ src/ast.rs
@@ -0,0 +1,139 @@
+use std::collections::{BTreeMap, BTreeSet};
+
+// Simple JavaScript AST
+use serde::{Deserialize, Serialize};
+#[derive(Debug, Clone, Copy, Serialize, Deserialize, PartialEq)]
+pub enum BinOp {
+    /// +
+    Add,
+    /// -
+    Sub,
+    /// *
+    Mul,
+    /// /
+    Div,
+    /// %
+    Mod,
+    /// ==
+    Eq,
+    /// !=
+    Ne,
+    /// ===
+    StrictEq,
+    /// !==
+    StrictNe,
+    /// <
+    Lt,
+    /// <=
+    Le,
+    /// >
+    Gt,
+    /// >=
+    Ge,
+    /// &&
+    And,
+    /// ||
+    Or,
+    /// in
+    In,
+    /// instanceof
+    InstanceOf,
+    /// a[b] or a.b (Known as Member)
+    Idx,
+}
+
+#[derive(Debug, Clone, Copy, Serialize, Deserialize, PartialEq)]
+pub enum UnOp {
+    /// !
+    Not,
+    /// -
+    Neg,
+    /// +
+    Pos,
+    /// typeof
+    TypeOf,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+pub enum Literal {
+    Number(f64),
+    String(String),
+    Boolean(bool),
+    Null,
+    Undefined,
+    Array(Vec<Expr>),
+    Object(BTreeMap<String, Expr>),
+    /// For error recovery
+    Error,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+pub enum Expr {
+    Literal(Literal),
+    BinOp(BinOp, Box<Expr>, Box<Expr>),
+    UnOp(UnOp, Box<Expr>),
+    Cond {
+        cond: Box<Expr>,
+        then: Box<Expr>,
+        r#else: Box<Expr>,
+    },
+    Ident(String),
+    Call {
+        func: Box<Expr>,
+        args: Vec<Expr>,
+    },
+    /// For error recovery
+    Error,
+}
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+pub struct Init {
+    pub name: String,
+    pub value: Expr,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+pub enum Stmt {
+    Expr(Expr),
+    If {
+        cond: Expr,
+        then: Box<Stmt>,
+        r#else: Option<Box<Stmt>>,
+    },
+    While {
+        cond: Expr,
+        body: Box<Stmt>,
+    },
+    Block(StmtList),
+    Let(Vec<Init>),
+    Const(Vec<Init>),
+    Function {
+        name: String,
+        params: Vec<String>,
+        body: Box<Stmt>,
+    },
+    /// `return` statement with an optional expression
+    /// Expr::Undefined if no expression is provided
+    Return(Expr),
+    Break,
+    Continue,
+    Empty,
+    /// For error recovery
+    Error,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+pub struct StmtList(pub Vec<Stmt>);
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+pub struct JavaScript {
+    pub body: StmtList,
+}
+
+/// Error when duplicate key is found in object literal
+pub fn k_v_list_to_object(list: Vec<(String, Expr)>) -> Literal {
+    let keys: BTreeSet<_> = list.iter().map(|(k, _)| k).collect();
+    if keys.len() != list.len() {
+        return Literal::Error;
+    }
+    Literal::Object(list.into_iter().collect())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use lalrpop_util::lalrpop_mod;
+
+lalrpop_mod!(pub js);
+mod ast;
+
 fn main() {
-    println!("Hello, world!");
+    unimplemented!();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        let mut errors = Vec::new();
+        let ast = match js::JavaScriptParser::new()
+            .parse(&mut errors, r#"1;"1\"5"+1;{-10};let pi = Math.acos(-1);"#)
+        {
+            Ok(ast) => ast,
+            Err(e) => {
+                eprintln!("Error: {:?}", e);
+                return;
+            }
+        };
+        println!("{}", serde_json::to_string_pretty(&ast).unwrap());
+        for e in errors {
+            eprintln!("Error: {:?}", e.error);
+        }
+    }
 }
