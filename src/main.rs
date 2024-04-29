f8ba5107bb425f376a6fe238fde38ac8de6f4102
yyhhenry <yyhhenry@foxmail.com>
2024-04-30 00:28:34 +0800

    Remove test

--- src/main.rs
+++ src/main.rs
@@ -112,26 +112,3 @@ fn main() -> Result<()> {
     }
     Ok(())
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test() {
-        let mut errors = Vec::new();
-        let ast = match js::JavaScriptParser::new()
-            .parse(&mut errors, r#"1;"1\"5"+1;{-10};let pi = Math.acos(-1);"#)
-        {
-            Ok(ast) => ast,
-            Err(e) => {
-                eprintln!("Error: {:?}", e);
-                return;
-            }
-        };
-        println!("{}", serde_json::to_string_pretty(&ast).unwrap());
-        for e in errors {
-            eprintln!("Error: {:?}", e.error);
-        }
-    }
-}
